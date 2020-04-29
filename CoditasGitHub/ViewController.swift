9eb34f1f20df2eabf701662174552ebdfb22b0ae
Ajay Ranekar <ajay.ranekar@desteksolutions.com>
2020-04-29 10:18:02 +0530

    Initial Commit

---
+++ CoditasGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoditasGitHub
+//
+//  Created by Apple on 28/04/20.
+//  Copyright © 2020 Ajay Ranekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
